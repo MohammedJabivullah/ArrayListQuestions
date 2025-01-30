c703033a8f62a2ac24d064da9b10f427489c69f4
gouse <ghouse08215@gmail.com>
2025-01-30 16:34:04 +0530

    Question1

---
+++ src/main.java
@@ -0,0 +1,15 @@
+public class main {
+    public static void main(String[] args) {
+        /*
+         * Write a program that returns the list of elements 
+         * that are present in the below given list and are divisible by 3.
+         * 
+         * 
+         * if number is divisible by 3 add them in list and return the arrayList
+         */
+        int a[] = {12,4,9,21,23,28,15,19,25,32,6};
+        
+        
+    }
+
+}
