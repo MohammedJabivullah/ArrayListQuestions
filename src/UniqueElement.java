a6fa10eba30c592382c317e16de5500921512a6c
gouse <ghouse08215@gmail.com>
2025-01-30 16:48:45 +0530

    uniqueelement

---
+++ src/UniqueElement.java
@@ -0,0 +1,10 @@
+public class UniqueElement {
+    public static void main(String[] args) {
+        /*
+         * Given an Array of integers A, every element in it is repeated twice except one, find that unique element.
+         */
+        int a[] = {4,2,2,1,4,1,3,8,9,8,9};
+        
+    }
+
+}
