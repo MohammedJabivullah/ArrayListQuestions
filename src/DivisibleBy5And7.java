e03d24530e06aaade764332d731db134a43320ff
gouse <ghouse08215@gmail.com>
2025-01-30 16:44:53 +0530

    divby5and7

---
+++ src/DivisibleBy5And7.java
@@ -0,0 +1,13 @@
+public class DivisibleBy5And7 {
+    public static void main(String[] args) {
+        /*
+         * Write a program that returns the list of elements 
+         * that are present in the below given list and are divisible by 5 and 7.
+         * 
+         * 
+         * if number is divisible by 5 and 7 add them in list and return the arrayList
+         */
+        int a[] = {5, 7, 70, 50, 35,105,175,15};
+    }
+
+}
