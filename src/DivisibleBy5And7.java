bbd610c2b194aec9e18b7d2449ea8c4380e3b987
MohammedJabivullah <mohammadjabivullah07@gmail.com>
2025-01-31 17:31:19 +0530

    Update DivisibleBy5And7.java

--- src/DivisibleBy5And7.java
+++ src/DivisibleBy5And7.java
@@ -8,6 +8,17 @@ public class DivisibleBy5And7 {
          * if number is divisible by 5 and 7 add them in list and return the arrayList
          */
         int a[] = {5, 7, 70, 50, 35,105,175,15};
+
+         ArrayList<Integer> A = new ArrayList<Integer>();
+        for(int i=0;i<a.length;i++) {
+            if((a[i]%5==0)&&(a[i]%7==0)){
+                A.add(a[i]);
+            }
+        }
+
+        for(int i=0;i<A.size();i++){
+            System.out.print(A.get(i)+" ");
+        }
     }
 
 }
