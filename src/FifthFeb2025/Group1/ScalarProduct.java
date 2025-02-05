859d35843967ed994e738ef99217c9de6db162b2
gouse <ghouse08215@gmail.com>
2025-02-05 19:12:36 +0530

    questions of 5th Feb

---
+++ src/FifthFeb2025/Group1/ScalarProduct.java
@@ -0,0 +1,21 @@
+package FifthFeb2025.Group1;
+
+public class ScalarProduct {
+    public static void main(String[] args) {
+        /*
+         * ou are given a matrix A and and an integer B, 
+         * you have to perform scalar multiplication of matrix A with an integer B
+         * 
+         * A = [[1, 2, 3],
+                [4, 5, 6],
+                [7, 8, 9]]
+            B = 2 
+            ---Answer array will be -------
+            [[2, 4, 6], 
+             [8, 10, 12], 
+             [14, 16, 18]]
+         */
+        
+    }
+
+}
