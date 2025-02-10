2b1b054ff900af6efac38259f781f107b6273383
gouse <ghouse08215@gmail.com>
2025-02-10 19:13:11 +0530

    10th Feb Question

---
+++ src/TenthFeb2025/RangeSum.java
@@ -0,0 +1,21 @@
+package TenthFeb2025;
+
+public class RangeSum {
+    public static void main(String[] args) {
+        /*
+         * Given an Array of Integers
+         * A[] = {5,7,9,2,1,-5,8,6,4};
+         * find sum of elements within a given Range in 2D Matrix
+        B[][] {{0,3}
+               {1,4}
+               {2,8}
+               {0,5}};
+               -----------Explanation------
+               {0,3} = 5+7+9+2       = 23
+               {1,4} = 7+9+2+1       = 19
+               {2,8} = 9+2+1-5+8+6+4 = 25
+               {0,5} = 5+7+9+2+1-5   = 18
+         */
+    }
+
+}
