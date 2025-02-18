c0c311f904c2337c97c86077c36cfdb8cc306b18
MohammedJabivullah <mohammadjabivullah07@gmail.com>
2025-02-18 18:12:16 +0530

    Answer after bit manuplation.

--- src/UniqueElement.java
+++ src/UniqueElement.java
@@ -4,18 +4,12 @@ public class UniqueElement {
          * Given an Array of integers A, every element in it is repeated twice except one, find that unique element.
          */
         int a[] = {4,2,2,1,4,1,3,8,9,8,9};
+
+        int U = 0;
         
         for(int i=0;i<a.length;i++){
-            int count = 0;
-            for(int j=0;j<a.length;j++){
-                if(a[i]==a[j]){
-                    count++;
-                }
-            }
-            if(count<2){
-                System.out.print(a[i]);
-                break;
-            }
+           U = U^a[i];
         }
+        System.out.println(U);
     }
 }
